d56ff44df44b09bcfde9876326465b3a8d56db11
Harigovind M <harigovindmankulam@gmail.com>
2026-03-20 16:48:25 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById("downloadCV").addEventListener("click", function () {
-  
   const link = document.createElement("a");
   link.href = "Harigovind_Resume.pdf"; // your CV file name
   link.download = "Harigovind_Resume.pdf";
@@ -7,15 +6,4 @@ document.getElementById("downloadCV").addEventListener("click", function () {
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
-
-});
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({
-        behavior: "smooth"
-      });
-  });
 });
